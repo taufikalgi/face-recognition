8ac262be12cddd9538b92a175c7be9ca1e99c849
Taufik <taufik.algi99@gmail.com>
2020-08-18 17:21:13 +0700

    Updates

---
+++ precache-manifest.f156736abfaf59503c48be1184230e0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941649a16d72a63d1bd56ad9456ba17d",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "f14ae82b56c146a013b3",
+    "url": "/face-recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cfb0a4d1571499cdfb12",
+    "url": "/face-recognition/static/css/main.12c0da47.chunk.css"
+  },
+  {
+    "revision": "f14ae82b56c146a013b3",
+    "url": "/face-recognition/static/js/2.d8b7f52e.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition/static/js/2.d8b7f52e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb0a4d1571499cdfb12",
+    "url": "/face-recognition/static/js/main.e5715901.chunk.js"
+  },
+  {
+    "revision": "b86f4757c9b31828b647",
+    "url": "/face-recognition/static/js/runtime-main.9817fe0c.js"
+  }
+]);
